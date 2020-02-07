6fc00cb9039bbecd178deb0ddec0ad8a608a922e
artem_vasiliev <vincere802@gmail.com>
2020-02-07 11:07:12 +0200

    add Navbar menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css'
+import Navbar from "./Navbar/Navbar";
 
 
 function Header() {
   return (
       <div className={styles.header}>
+          <div className={styles.container}>
+             <Navbar/>
+          </div>
 
       </div>
   );
