c3074aab3aaf5d527c623dbd025e0522affba63c
zionjke <vincere802@gmail.com>
2020-02-07 23:42:34 +0200

    styled Slogan Component

--- src/components/Slogan/Slogan.js
+++ src/components/Slogan/Slogan.js
@@ -5,7 +5,12 @@ import styles from './Slogan.module.css'
 function Slogan() {
   return (
       <div className={styles.slogan}>
-
+            <div className={styles.container}>
+                <div className={styles.slogan_block}>
+                    <span>Рассматриваю варианты удаленной работы</span>
+                    <button>Нанять меня</button>
+                </div>
+            </div>
       </div>
   );
 }
