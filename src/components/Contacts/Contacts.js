40e784559823c2a7ec86e1f26d797663061f59a4
zionjke <vincere802@gmail.com>
2020-02-08 00:02:41 +0200

    styled Contacts component

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -5,7 +5,22 @@ import styles from './Contacts.module.css'
 function Contacts() {
   return (
       <div className={styles.contacts}>
+            <div className={styles.container}>
+                <div className={styles.contacts_block}>
+                    <span>Контакты</span>
+                    <div className={styles.contacts_block_form}>
+                        <form>
+                            <input type="text"/>
+                            <input type="text"/>
+                            <textarea ></textarea>
+                        </form>
+                    </div>
+                    <button>
+                        Отправить
+                    </button>
 
+                </div>
+            </div>
       </div>
   );
 }
