ee32bb8cc44ea594c62305a4700f149d49db3f1b
artem_vasiliev <vincere802@gmail.com>
2020-02-07 12:02:29 +0200

    add structure in Main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,16 @@ import styles from './Main.module.css'
 function Main() {
   return (
       <div className={styles.main}>
-
+          <div className={styles.container}>
+                <div className={styles.greeting}>
+                    <span>Привет</span>
+                    <span>Меня зовут Васильев Артем</span>
+                    <span>Я front-end разработчик</span>
+                    <div className={styles.photo}>
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+          </div>
       </div>
   );
 }
