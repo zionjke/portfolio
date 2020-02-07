857c14a215c51d68474b779f37630f229cc1fc3e
zionjke <vincere802@gmail.com>
2020-02-07 21:43:16 +0200

    create and add Item component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import styles from './Skills.module.css'
+import Item from "./Item/Item";
 
 
 function Skills() {
-  return (
-      <div className={styles.skills}>
+    return (
+        <div className={styles.skills}>
             <div className={styles.container}>
                 <div className={styles.myskills}>
                     <span>Мои Скиллы</span>
                 </div>
                 <div className={styles.myskill_items}>
-                        <div className={styles.myskill_item}>
-                            <div className={styles.myskill_item_image}>
-                                <img src="" alt=""/>
-                            </div>
-                            <span>REACT</span>
-                            <div className={styles.myskill_item_description}>
-                                    <span>Подробное описание навыка</span>
-                            </div>
-
-                        </div>
+                    <Item/>
+                    <Item/>
+                    <Item/>
                 </div>
             </div>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default Skills;
