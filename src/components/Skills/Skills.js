c8c6b9619566087c3d1d67e44db2c8faf43b3aba
artem_vasiliev <vincere802@gmail.com>
2020-02-07 16:43:23 +0200

    add div myskills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,7 +5,11 @@ import styles from './Skills.module.css'
 function Skills() {
   return (
       <div className={styles.skills}>
-
+            <div className={styles.container}>
+                <div className={styles.myskills}>
+                    <span>Мои Скиллы</span>
+                </div>
+            </div>
       </div>
   );
 }
