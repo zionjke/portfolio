500f8df92a1bb66b807bfc18e5fcf1baf0ec7014
artem_vasiliev <vincere802@gmail.com>
2020-02-07 12:52:48 +0200

    add span's and photo in Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,20 +3,21 @@ import styles from './Main.module.css'
 
 
 function Main() {
-  return (
-      <div className={styles.main}>
-          <div className={styles.container}>
+    return (
+        <div className={styles.main}>
+            <div className={styles.container}>
                 <div className={styles.greeting}>
                     <span>Привет</span>
                     <span>Меня зовут Васильев Артем</span>
                     <span>Я front-end разработчик</span>
-                    <div className={styles.photo}>
-                        <img src="" alt=""/>
-                    </div>
+
                 </div>
-          </div>
-      </div>
-  );
+                <div className={styles.photo}>
+                    <img src="" alt=""/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Main;
