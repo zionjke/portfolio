7f047b95046f2487cf9ec0e20638b18d474f3f2b
zionjke <vincere802@gmail.com>
2020-02-09 17:24:24 +0200

    styled Footer component v

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,20 @@ import styles from './Footer.module.css'
 function Footer() {
   return (
       <div className={styles.footer}>
+          <div className={styles.container}>
+                <div className={styles.footer_block}>
+                    <span>Иван Иванов</span>
+                    <div className={styles.footer_block_sociallinks}>
+
+                            <div className={styles.footer_block_sociallinks_item}></div>
+                            <div className={styles.footer_block_sociallinks_item}></div>
+                            <div className={styles.footer_block_sociallinks_item}></div>
+                            <div className={styles.footer_block_sociallinks_item}></div>
+
+                    </div>
+                    <span>2020 Все права защищены</span>
+                </div>
+          </div>
 
       </div>
   );
